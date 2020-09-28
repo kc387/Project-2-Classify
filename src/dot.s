77dcc15ce5073f9efd42a560cdc5ab683f623ace
Kathleen Chen <kathleen.chen686@duke.edu>
2020-09-28 05:52:39 -0400

    dot

--- src/dot.s
+++ src/dot.s
@@ -20,7 +20,7 @@
 dot:
 
     # Prologue
-	addi sp, sp, -32
+	addi sp, sp, -36
     sw ra, 0(sp)
     sw s0, 4(sp)
     sw s1, 8(sp)
@@ -30,6 +30,7 @@ dot:
     sw s5, 24(sp)
     sw s6, 28(sp)
     sw s7, 32(sp)
+    sw s8, 36(sp)
     
     add s0, a0, x0 # s0 -> pointer to vector v0
     add s1, a1, x0 # s1 -> pointer to vector v1
@@ -40,23 +41,21 @@ dot:
 loop_start:
 	li s5 0 # pointer thing for indexing thing
     addi t1, t1, 4
-    mul t2, s3, t1
-    mul t3, s4, t1
 
 
 loop_continue:
 	bge s5 s2 loop_end 
     lw s6 0(s0) # get value at v0[0] and stick it in s6 YAY
     lw s7 0(s1) # get value at v1[0] and stick it in s7 YAY
-    mul t0, s6, s7 # t0 = s6 * s7
-    add a0, a0, t0 # add t0 to a0
+    mul s8, s6, s7 # t0 = s6 * s7
+    add a0, a0, s8 # add t0 to a0
     
     sw s6 0(s0)
     sw s7 0(s0)
-    #add s0, s0, t2
-    addi s0, s0, 4
-    #add s1, s1, t3
-    addi s1, s1, 4
+    mul t2, s3, t1
+    mul t3, s4, t1
+    add s0, s0, t2
+    add s1, s1, t3
     addi s5, s5, 1 # index increases by 1
     j loop_continue
 
@@ -71,7 +70,8 @@ loop_end:
     lw s5, 24(sp)
     lw s6, 28(sp)
     lw s7, 32(sp)
-    addi sp, sp, 32
+    lw s8, 36(sp)
+    addi sp, sp, 36
 
     jr ra
 
