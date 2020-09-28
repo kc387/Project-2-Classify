64278c57bfa3f7cc03448a44e32a5ed89e397050
Kathleen Chen <kathleen.chen686@duke.edu>
2020-09-28 03:09:42 -0400

    relu

--- src/relu.s
+++ src/relu.s
@@ -14,23 +14,34 @@
 # ==============================================================================
 relu:
     # Prologue
+    addi sp, sp, -20
+    sw ra, 0(sp)
+    sw s0, 4(sp)
+    sw s1, 8(sp)
+    sw s2, 12(sp)
+    sw s3, 16(sp)
 
+    add s0, s0, x0 # s0 -> pointer to array
+    add s1, s1, x0 # s1 -> number of elements in array
 
 loop_start:
-    
-
-
-
-
-
-
+    li s2 0 
 
 loop_continue:
-
-
+    bge s2 s1 loop_end
+    lw s3 0(x0)
+    bit x0 s3 greater_than_zero # if s3 > 0 goto greater_than_zero
+    li s3 0
 
 loop_end:
+    lw ra, 0(sp)
+    lw s0, 4(sp)
+    lw s1, 8(sp)
+    lw s2, 12(sp)
+    lw s3, 16(sp)
+    addi sp, sp, 20
 
+    jr ra
 
     # Epilogue
 
