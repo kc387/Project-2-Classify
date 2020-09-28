dd77abdff0ec830be2edcefff0fe0b3444fa59dc
Kathleen Chen <kathleen.chen686@duke.edu>
2020-09-28 04:16:04 -0400

    argmax

--- src/argmax.s
+++ src/argmax.s
@@ -17,23 +17,44 @@
 argmax:
 
     # Prologue
-
+    addi sp, sp, -20
+    sw ra, 0(sp)
+    sw s0, 4(sp)
+    sw s1, 8(sp)
+    sw s2, 12(sp)
+    sw s3, 16(sp)
+    sw s4, 20(sp)
+    
+    add s0, a0, x0 # s0 -> pointer to vector
+    add s1, a1, x0 # s1 -> number of elements in vector
 
 loop_start:
-
-
-
-
-
-
-
+	li s2 0 # pointer thing for indexing thing
+    lw s4 0(s0) # s4 represents greatest current value
 
 loop_continue:
-
-
+	bge s2 s1 loop_end 
+	lw s3 0(s0) # get value at vector[0] and stick it in s3 YAY
+    blt s3 s4 less_than # if s3 < s4 goto less_than
+    add s4, s3, x0 # s4 = s3
+    add a0, s2, x0 # a0 = s2
+    
+less_than:
+	sw s3 0(s0)
+    addi s0 s0 4
+    addi s2 s2 1
+    j loop_continue
 
 loop_end:
-    
+    lw ra, 0(sp)
+    lw s0, 4(sp)
+    lw s1, 8(sp)
+    lw s2, 12(sp)
+    lw s3, 16(sp)
+    lw s4, 20(sp)
+    addi sp, sp, 20
+
+    jr ra
 
     # Epilogue
 
