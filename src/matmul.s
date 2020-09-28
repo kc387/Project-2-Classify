a75a146f05c76f0ad442c0f2594c0e26f64bca74
Kathleen Chen <kathleen.chen686@duke.edu>
2020-09-28 13:33:03 -0400

    matmul

--- src/matmul.s
+++ src/matmul.s
@@ -24,11 +24,8 @@
 # =======================================================
 matmul:
 
-    # Error checks
-
-
     # Prologue
-    addi sp, sp, -36
+	addi sp, sp, -44
     sw ra, 0(sp)
     sw s0, 4(sp)
     sw s1, 8(sp)
@@ -39,32 +36,130 @@ matmul:
     sw s6, 28(sp)
     sw s7, 32(sp)
     sw s8, 36(sp)
+    sw s9, 40(sp)
+    sw s10, 44(sp)
+    
+    add s0, a0, x0 # s0 -> pointer to start of m0 (left matrix)
+    add s1, a1, x0 # s1 -> # rows of m0
+    add s2, a2, x0 # s2 -> # cols of m0
+    add s3, a3, x0 # s3 -> pointer to start of m1 (right matrix)
+    add s4, a4, x0 # s4 -> # rows of m1
+    add s5, a5, x0 # s5 -> # cols of m1
+    add s6, a6, x0 # s6 -> pointer to start of d (return matrix)
+    
+    # Error checks
+    li t0 1
+    blt s1 t0 error2
+    blt s2 t0 error2
+    blt s4 t0 error3
+    blt s5 t0 error3
+    bne s2 s4 error4
 
+loop_start:
+	li s7 0 # s7 -> which row of m0 you are on
+    li s8 0 # s8 -> which col of m1 you are on
+    li t1 1 # stride of m0
+    li t2 0
+    add t2, t2, s5 # stride of m1
+    
 outer_loop_start:
-
-
-
+	lw s9 0(s0) # get value at m0[0] and stick it in s9 YAY
+    bge s7 s1 end
 
 inner_loop_start:
-
-
-
-
-
-
-
-
-
-
-
-
-inner_loop_end:
-
-
-
-
+	lw s10 0(s3) # get value at m1[0] and stick it in s10 YAY
+    bge s8 s5 outer_loop_start
+    # dot
+    mv a0 s9
+    mv a1 s10
+    mv a2 s2
+    mv a3 t1
+    mv a4 t2
+    jal ra dot
+	sw s6 0(a0) # store dot in d[0]
+    addi s6, s6, 4 # shift over one
+    sw s10 0(s3)
+    
+    addi s10, s10, 4 # move over
+    addi s8, s8, 1
+    j outer_loop_end
+    
 outer_loop_end:
+	sw s9 0(s0)
+    li t4 4
+    mul t5, s2, t4
+    add s9, s9, t5
+    addi s7, s7, 1
+    j outer_loop_start
+    
+end:
+	lw ra, 0(sp)
+    lw s0, 4(sp)
+    lw s1, 8(sp)
+    lw s2, 12(sp)
+    lw s3, 16(sp)
+    lw s4, 20(sp)
+    lw s5, 24(sp)
+    lw s6, 28(sp)
+    lw s7, 32(sp)
+    lw s8, 36(sp)
+    lw s9, 40(sp)
+    lw s10, 44(sp)
+    addi sp, sp, 44
 
+	jr ra
+
+	# error
+error2:
+	li a1 2
+    lw ra, 0(sp)
+    lw s0, 4(sp)
+    lw s1, 8(sp)
+    lw s2, 12(sp)
+    lw s3, 16(sp)
+    lw s4, 20(sp)
+    lw s5, 24(sp)
+    lw s6, 28(sp)
+    lw s7, 32(sp)
+    lw s8, 36(sp)
+    lw s9, 40(sp)
+    lw s10, 44(sp)
+    addi sp, sp, 44
+    jal exit2
+    
+error3:
+	li a1 3
+    lw ra, 0(sp)
+    lw s0, 4(sp)
+    lw s1, 8(sp)
+    lw s2, 12(sp)
+    lw s3, 16(sp)
+    lw s4, 20(sp)
+    lw s5, 24(sp)
+    lw s6, 28(sp)
+    lw s7, 32(sp)
+    lw s8, 36(sp)
+    lw s9, 40(sp)
+    lw s10, 44(sp)
+    addi sp, sp, 44
+    jal exit2
+    
+error4:
+	li a1 4
+    lw ra, 0(sp)
+    lw s0, 4(sp)
+    lw s1, 8(sp)
+    lw s2, 12(sp)
+    lw s3, 16(sp)
+    lw s4, 20(sp)
+    lw s5, 24(sp)
+    lw s6, 28(sp)
+    lw s7, 32(sp)
+    lw s8, 36(sp)
+    lw s9, 40(sp)
+    lw s10, 44(sp)
+    addi sp, sp, 44
+    jal exit2
 
     # Epilogue
     
