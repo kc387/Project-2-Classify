b507e5fac6a2376757b052c2cef3a7ebef2085b9
Kathleen Chen <kathleen.chen686@duke.edu>
2020-09-29 13:15:15 -0400

    read matrix

--- src/read_matrix.s
+++ src/read_matrix.s
@@ -25,8 +25,8 @@
 # ==============================================================================
 read_matrix:
 
-    # Prologue
-	addi sp, sp, -40
+	# Prologue
+	addi sp, sp, -44
     sw ra, 0(sp)
     sw s0, 4(sp)
     sw s1, 8(sp)
@@ -37,41 +37,82 @@ read_matrix:
     sw s6, 28(sp)
     sw s7, 32(sp)
     sw s8, 36(sp)
-	
+    sw s9, 40(sp)
+
     add s0, a0, x0 # pointer to string
     add s1, a1, x0 # pointer to int to store rows
     add s2, a2, x0 # pointer to int to store cols
-    
-	
-	# fopen
+
+    # open file
     mv a1 s0
     mv a2 x0
-	jal ra fopen
-    add s3, a0, x0 # store a0
+    jal ra fopen
 
-	# fread rows
-    mv a1 s3
-    mv a2 s1
-    li t1 4
-    mv a3 t1
-    jal ra fread
+	# check for error (fopen error)
+    li t1 -1 
+    beq a0, t1, error50 # don't have file descriptor
 
-	# fread col
-    mv a1 s3
-    mv a2 s2
-    li t1 4
-    mv a3 t1
+    mv s3 a0 # file descriptor
+
+    # malloc sadness
+    li a0 8
+    jal ra malloc
+    
+    # check for error (malloc doesn't work right) -> malloced 0 space
+    beq a0, x0, error48 
+    
+    mv s4 a0 # size of matrix (8 spaces)
+
+	# fread cols & rows
+    mv a1 s3 
+    mv a2 s4 
+    li a3 8
     jal ra fread
     
-    # fread col
+    # check for error (doesn't read 8 bytes like it was supposed to)
+    li a3 8 
+    bne a0, a3, error51 
+
+    lw s5, 0(s4) 
+    lw s6, 4(s4) 
+    sw s5, 0(s1) #set row
+    sw s6, 0(s2) #set col
+
+    # malloc sadness 2.0
+    mul a0, s5, s6
+    slli a0, a0, 2
+    jal ra malloc
+    
+    # check for error (malloc wrong -> 0 space allocated)
+    beq a0, x0, error48 
+    
+    mv s7 a0 # matrix pointer
+
+	# fread rest of matrix
     mv a1 s3
-    mv a2 a0
-    li t1 4
-    mul s4, s1, s2
-    mul s5, s4, t1
-    mv a3 s5
+    mv a2 s7
+    mul a3, s5, s6
+    slli a3, a3, 2 
     jal ra fread
 
+	# check for error (fread doesn't go right -> size read isn't right)
+    mul a3, s5, s6 
+    slli a3, a3, 2 
+    bne a0, a3, error51 
+    
+    # close file
+    mv a1, s3
+    jal ra fclose
+    
+    # check for error (doesn't close right)
+    bne a0, x0, error52 
+
+    j done
+
+done:
+    mv a0 s7
+
+    #Epilogue
     lw ra, 0(sp)
     lw s0, 4(sp)
     lw s1, 8(sp)
@@ -82,9 +123,25 @@ read_matrix:
     lw s6, 28(sp)
     lw s7, 32(sp)
     lw s8, 36(sp)
-    addi sp, sp, 40
+    lw s9, 40(sp)
+    addi sp, sp, 44
     
-    # Epilogue
-
-
+    jr ra
+    
     ret
+
+error48:
+    li a1 48
+    jal exit2
+
+error50:
+    li a1 50
+    jal exit2
+
+error51:
+    li a1 51
+    jal exit2
+
+error52:
+    li a1 52
+    jal exit2
--- tests/assembly/test_read_matrix.s
+++ tests/assembly/test_read_matrix.s
@@ -2,16 +2,38 @@
 .import ../../src/utils.s
 
 .data
-file_path: .asciiz "inputs/test_read_matrix/test_input.bin"
+file_path: .asciiz "../../test_input.bin"
 
 .text
 main:
-    # Read matrix into memory
+    # Read matrix into memory
 
+    la s0 file_path
+ebreak
+    
+    li a0 4
+    jal ra malloc
+    mv s1 a0
 
+    li a0 4
+    jal ra malloc
+    mv s2 a0
 
-    # Print out elements of matrix
+    mv a0 s0
+    mv a1 s1
+    mv a2 s2
 
+    jal ra read_matrix
+    
+    ebreak
 
+    # Print out elements of matrix
 
-    # Terminate the program
+    mv a0 a0
+    lw a2 4(a1)
+    lw a1 0(a1)
+    
+    jal print_int_array
+
+    # Terminate the program
+    jal exit
