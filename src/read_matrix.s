dff6aace33a83caf1dbfaa14bef41d1d2d304083
Kathleen Chen <kathleen.chen686@duke.edu>
2020-09-28 23:18:23 -0400

    kathleen dumb

--- src/read_matrix.s
+++ src/read_matrix.s
@@ -26,15 +26,64 @@
 read_matrix:
 
     # Prologue
+	addi sp, sp, -40
+    sw ra, 0(sp)
+    sw s0, 4(sp)
+    sw s1, 8(sp)
+    sw s2, 12(sp)
+    sw s3, 16(sp)
+    sw s4, 20(sp)
+    sw s5, 24(sp)
+    sw s6, 28(sp)
+    sw s7, 32(sp)
+    sw s8, 36(sp)
 	
+    add s0, a0, x0 # pointer to string
+    add s1, a1, x0 # pointer to int to store rows
+    add s2, a2, x0 # pointer to int to store cols
+    
+	
+	# fopen
+    mv a1 s0
+    mv a2 x0
+	jal ra fopen
+    add s3, a0, x0 # store a0
 
+	# fread rows
+    mv a1 s3
+    mv a2 s1
+    li t1 4
+    mv a3 t1
+    jal ra fread
 
+	# fread col
+    mv a1 s3
+    mv a2 s2
+    li t1 4
+    mv a3 t1
+    jal ra fread
+    
+    # fread col
+    mv a1 s3
+    mv a2 a0
+    li t1 4
+    mul s4, s1, s2
+    mul s5, s4, t1
+    mv a3 s5
+    jal ra fread
 
-
-
-
-
-
+    lw ra, 0(sp)
+    lw s0, 4(sp)
+    lw s1, 8(sp)
+    lw s2, 12(sp)
+    lw s3, 16(sp)
+    lw s4, 20(sp)
+    lw s5, 24(sp)
+    lw s6, 28(sp)
+    lw s7, 32(sp)
+    lw s8, 36(sp)
+    addi sp, sp, 40
+    
     # Epilogue
 
 
